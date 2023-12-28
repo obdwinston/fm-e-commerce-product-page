87f03888a478283129862c552f2a36382d34f737
Winston Ong <obdwinston@gmail.com>
2023-12-28 22:02:01 +0800

    added overlay

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileLinks = document.querySelector(".links");
+const overlay = document.querySelector(".overlay");
 
 mobileMenu.addEventListener("click", function () {
   mobileMenu.classList.toggle("mobile-menu-close");
   mobileLinks.classList.toggle("show-links");
+
+  if (mobileMenu.classList.contains("mobile-menu-close")) {
+    overlay.style.display = "block"; // display overlay
+    document.body.style.overflow = "hidden"; // prevent scrolling
+  } else {
+    overlay.style.display = "none";
+    document.body.style.overflow = "visible";
+  }
 });
